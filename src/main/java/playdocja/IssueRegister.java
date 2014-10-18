2d546526df849cddf5e78ad758ff94bb8370d545
garbagetown <garbagetown@gmail.com>
2014-10-18 17:02:16 +0900

    modify method apis and remove unnecessary methods

--- src/main/java/playdocja/IssueRegister.java
+++ src/main/java/playdocja/IssueRegister.java
@@ -3,39 +3,62 @@ package playdocja;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.log4j.Logger;
 import org.kohsuke.github.GHIssueBuilder;
+import org.kohsuke.github.GHIssueState;
+import org.kohsuke.github.GHMilestone;
 import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GitHub;
 
 public class IssueRegister {
 
     private static Logger logger = Logger.getLogger(IssueRegister.class.getName());
-    private static String baseurl = "https://github.com/garbagetown/playdocja/blob/2.2.0/documentation";
 
     public static final String MARKDOWN_EXT = ".md";
     
     public static final String LABEL_UPDATE = "update";
     public static final String LABEL_NEW = "new";
 
+    private Path basepath;
+    private Path oldpath;
+    private Path newpath;
+    private Properties prop;
+    
     /**
      * 
      * @param basepath
      * @param olddir
      * @param newdir
+     */
+    public IssueRegister(Path basepath, String olddir, String newdir) {
+        this.basepath = basepath;
+        this.oldpath = basepath.resolve(olddir).toAbsolutePath();
+        this.newpath = basepath.resolve(newdir).toAbsolutePath();
+        this.prop = new Properties();
+        try {
+            InputStream stream = IssueRegister.class.getResourceAsStream("../github.properties");
+            prop.load(stream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    /**
+     * 
      * @return
      */
-    public List<Issue> getDiffsAsIssues(Path basepath, String olddir, String newdir) {
-
-        Path oldpath = basepath.resolve(olddir).toAbsolutePath();
-        Path newpath = basepath.resolve(newdir).toAbsolutePath();
+    public List<Issue> getDiffsAsIssues() {
 
         String command = String.format("diff -qr %s %s", oldpath, newpath);
         logger.debug(command);
@@ -51,7 +74,7 @@ public class IssueRegister {
             if (m.find()) {
                 String path = m.group(1);
                 if (path.endsWith(MARKDOWN_EXT)) {
-                    issues.add(toUpdate(oldpath, newpath, path));
+                    issues.add(toUpdate(path));
                 }
                 continue;
             }
@@ -60,7 +83,7 @@ public class IssueRegister {
             m = p.matcher(result.replace(": ", "/"));
             if (m.find()) {
                 String path = m.group(1);
-                issues.addAll(toNew(newpath, path));
+                issues.addAll(toNew(path));
                 continue;
             }
         }
@@ -69,20 +92,26 @@ public class IssueRegister {
     
     /**
      * 
-     * @param oldpath
-     * @param newpath
      * @param path
      * @return
      */
-    private Issue toUpdate(Path oldpath, Path newpath, String path) {
+    private Issue toUpdate(String path) {
         logger.debug(String.format("[%-6s] %s/%s", LABEL_UPDATE, newpath, path));
 
-        String command = String.format("diff -u %s/%s /%s/%s", oldpath, path, newpath, path);
+        String baseurl = prop.getProperty("baseurl");
+        String olddir = basepath.toAbsolutePath().relativize(oldpath).toString();
+        String newdir = basepath.toAbsolutePath().relativize(newpath).toString();
+        String oldtitle = olddir + "/" + path;
+        String newtitle = newdir + "/" + path;
         
+        StringBuilder body = new StringBuilder();
+        body.append(String.format("- %s/%s\n", baseurl, newtitle));
+        body.append(String.format("- diffs between [%s](%s/%s) ", oldtitle, baseurl, oldtitle));
+        body.append(String.format("and [%s](%s/%s).\n", newtitle, baseurl, newtitle));
+        
+        String command = String.format("diff -u %s/%s /%s/%s", oldpath, path, newpath, path);
         List<String> results = execute(command);
 
-        StringBuilder body = new StringBuilder();
-        body.append(String.format("- %s/%s\n", baseurl, path));
         body.append("```\n");
         for (String result : results) {
             result = result.replace("```", "'''");
@@ -96,18 +125,20 @@ public class IssueRegister {
     
     /**
      * 
-     * @param newpath
      * @param path
      * @return
      */
-    private List<Issue> toNew(Path newpath, String path) {
+    private List<Issue> toNew(String path) {
         logger.debug(String.format("[%-6s] %s/%s", LABEL_NEW, newpath, path));
+
+        String baseurl = prop.getProperty("baseurl");
+        String newdir = basepath.toAbsolutePath().relativize(newpath).toString();
 
         List<File> files = findFiles(newpath.resolve(path).toFile());
         List<Issue> issues = new ArrayList<>();
         for (File file : files) {
             path = newpath.relativize(file.toPath()).toString();
-            String body = String.format("- %s/%s", baseurl, path);
+            String body = String.format("- %s/%s/%s", baseurl, newdir, path);
             issues.add(new Issue(LABEL_NEW, path, body));
         }
         return issues;
@@ -152,125 +183,97 @@ public class IssueRegister {
         }
         return lines;
     }
-    
-    public static void main(String[] args) throws IOException {
 
-        String basepath = args[0];
-        String oldver = args[1];
-        String newver = args[2];
-
-        if (basepath.endsWith("/")) {
-            basepath = basepath.substring(0, basepath.length() - 1);
+    /**
+     * 
+     * @return
+     */
+    public GHRepository getRepository() {
+        String l = prop.getProperty("login");
+        String p = prop.getProperty("password");
+        String o = prop.getProperty("organization");
+        String r = prop.getProperty("repository");
+        try {
+            GitHub github = GitHub.connectUsingPassword(l, p);
+            GHRepository repository = github.getRepository(o + "/" + r);
+            return repository;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        String command = String.format("diff -qr %s/%s %s/%s", basepath, oldver, basepath, newver);
-        logger.debug(command);
+    }
 
-        List<String> results = exec(command);
-        List<Issue> issues = new ArrayList<Issue>();
-        for (String result : results) {
-            if (result.endsWith(".DS_Store")) {
-                continue;
-            }
-            if (result.endsWith(": code")) {
-                continue;
-            }
-            Matcher deleted = Pattern.compile(String.format("^Only in %s/%s/(.+)", basepath, oldver)).matcher(result);
-            Matcher added = Pattern.compile(String.format("^Only in %s/%s/(.+)", basepath, newver)).matcher(result);
-            Matcher updated = Pattern.compile(String.format("^Files %s/%s/(.+) and %s/%s/(.+) differ$", basepath, oldver, basepath, newver)).matcher(result);
-            if (deleted.find()) {
-                issues.add(toDeleteIssue(oldver, deleted.group(1).replace(": ", "/")));
-            } else if (added.find()) {
-                issues.add(toNewIssue(newver, added.group(1).replace(": ", "/")));
-            } else if (updated.find()) {
-                issues.add(toUpdateIssue(basepath, oldver, newver, updated.group(1)));
-            } else {
-                throw new RuntimeException(result);
+    /**
+     * 
+     * @param issues
+     * @throws IOException 
+     * @throws NumberFormatException 
+     */
+    public void registerIssues(List<Issue> issues) throws NumberFormatException, IOException {
+        
+        GHRepository repository = getRepository();
+        
+        String s = prop.getProperty("milestone");
+        int n = -1;
+        
+        Iterator<GHMilestone> it = repository.listMilestones(GHIssueState.OPEN).iterator();
+        while (it.hasNext()) {
+            GHMilestone m = it.next();
+            logger.debug(m.getNumber() + ":" + m.getTitle());
+            if (m.getTitle().equals(s)) {
+                n = m.getNumber();
             }
         }
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Enter username: ");
-        String login = reader.readLine();
-        System.out.print("Enter password: ");
-        String password = reader.readLine();
-        System.out.print("Enter repository: ");
-        String name = reader.readLine();
-        System.out.print("Enter milestone number: ");
-        String number = reader.readLine();
-        reader.close();
-
-        GitHub github = GitHub.connectUsingPassword(login, password);
-        GHRepository repository = github.getRepository(String.format("%s/%s", login, name));
+        if (n < 0) {
+            throw new RuntimeException();
+        }
+        GHMilestone milestone = repository.getMilestone(n);
+        
         for (Issue issue : issues) {
-            if (issue == null) {
-                continue;
-            }
             GHIssueBuilder builder = repository.createIssue(issue.path);
-            builder.milestone(repository.getMilestone(Integer.parseInt(number)));
+            builder.milestone(milestone);
             builder.label(issue.label);
             builder.body(issue.body);
             builder.create();
             logger.debug(String.format("create %s issue: %s", issue.label, issue.path));
         }
     }
-
+    
     /**
      * 
-     * @param login
-     * @param password
-     * @param organization
-     * @param repository
-     * @return
+     * @param args
+     * @throws IOException
      */
-    public GHRepository getRepository(String login, String password, String organization, String repository) {
-        try {
-            GitHub g = GitHub.connectUsingPassword(login, password);
-            GHRepository r = g.getRepository(organization + "/" + repository);
-            return r;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    public static void main(String[] args) throws IOException {
+
+        Path basepath = Paths.get(args[0]);
+        String olddir = args[1];
+        String newdir = args[2];
+        
+        IssueRegister register = new IssueRegister(basepath, olddir, newdir);
+        
+        List<Issue> issues = register.getDiffsAsIssues();
+        int u = 0;
+        int n = 0;
+        for (Issue issue : issues) {
+            logger.info(String.format("[%-6s] %s", issue.label, issue.path));
+            switch (issue.label) {
+            case LABEL_UPDATE:
+                u++;
+                break;
+            case LABEL_NEW:
+                n++;
+                break;
+            default:
+                break;
+            }
         }
-    }
 
-    private static Issue toDeleteIssue(String oldver, String path) throws IOException {
-        logger.debug(String.format("[%-7s] %s/%s", "DELETED", oldver, path));
-        return null;
-    }
-
-    private static Issue toNewIssue(String newver, String path) throws IOException {
-        logger.debug(String.format("[%-7s] %s/%s", "ADDED", newver, path));
-        String body = String.format("- %s/%s/%s", baseurl, newver, path);
-        return new Issue("new", path, body);
-    }
-
-    private static Issue toUpdateIssue(String basepath, String oldver, String newver, String path) throws IOException {
-        logger.debug(String.format("[%-7s] %s/%s", "UPDATED", newver, path));
-
-        String command = String.format("diff -u %s/%s/%s %s/%s/%s", basepath, oldver, path, basepath, newver, path);
-        List<String> results = exec(command);
-
-        StringBuilder body = new StringBuilder();
-        body.append(String.format("- %s/%s/%s\n", baseurl, newver, path));
-        body.append("```\n");
-        for (String result : results) {
-            result = result.replace("```", "'''");
-            body.append(result);
-            body.append("\n");
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            logger.warn(String.format("%d update issues and %d new issues will be created: [y/N]", u, n));
+            String answer = reader.readLine();
+            if (answer.equals("y")) {
+                register.registerIssues(issues);
+            }
         }
-        body.append("\n```");
-
-        return new Issue("update", path, body.toString());
-    }
-
-    private static List<String> exec(String command) throws IOException {
-        Process p = Runtime.getRuntime().exec(command);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
-        String line;
-        List<String> lines = new ArrayList<String>();
-        while ((line = reader.readLine()) != null) {
-            lines.add(line);
-        }
-        reader.close();
-        return lines;
     }
 }
--- src/test/java/playdocja/IssueRegisterTest.java
+++ src/test/java/playdocja/IssueRegisterTest.java
@@ -3,12 +3,9 @@ package playdocja;
 import static org.hamcrest.core.Is.*;
 import static org.junit.Assert.*;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Properties;
 
 import org.junit.After;
 import org.junit.Before;
@@ -90,23 +87,24 @@ public class IssueRegisterTest {
      * @return
      */
     private List<Issue> getDiffsAsIssues(String dir) {
+        return issueRegister(dir).getDiffsAsIssues();
+    }
+
+    /**
+     * 
+     * @param dir
+     * @return
+     */
+    private IssueRegister issueRegister(String dir) {
         Path basepath = Paths.get("src/test/resources/issue/", dir);
         String olddir = "old";
         String newdir = "new";
-        return new IssueRegister().getDiffsAsIssues(basepath, olddir, newdir);
+        return new IssueRegister(basepath, olddir, newdir);
     }
-
+    
     @Test
-    public void getRepository() throws IOException {
-        Properties prop = new Properties();
-        try (InputStream stream = this.getClass().getResourceAsStream("../github.properties")) {
-            prop.load(stream);
-        }
-        String l = prop.getProperty("login");
-        String p = prop.getProperty("password");
-        String o = prop.getProperty("login");
-        String r = prop.getProperty("repository");
-        GHRepository repository = new IssueRegister().getRepository(l, p, o, r);
+    public void getRepository() {
+        GHRepository repository = issueRegister("test1").getRepository();
         assertNotNull(repository);
     }
 
